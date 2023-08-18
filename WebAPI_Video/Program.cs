2b44415f58acd257a285ae38a981f33466f56266
Crislaine Luana <92652629+CrislaineLuana@users.noreply.github.com>
2023-08-18 18:38:37 -0300

    Incluindo Cors

--- WebAPI_Video/Program.cs
+++ WebAPI_Video/Program.cs
@@ -16,6 +16,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("funcionariosApp", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,7 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("funcionariosApp");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
